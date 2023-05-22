0a74823a00990fd3a20cdae9160da8e9cf01eb24
Md Ashikur Rahman <arshakib51@gmail.com>
2023-05-22 10:58:41 +0600

    Add endless version of game functionality with UI

--- rps.js
+++ rps.js
@@ -2,12 +2,15 @@ function getComputerChoice(){
     let n = Math.floor(Math.random() * 3) + 1;
     switch(n){
         case 1:
+            computerTurn.textContent = '✊';
             return 'rock';
             break;
         case 2:
+            computerTurn.textContent = '✋';
             return 'paper';
             break;
         case 3:
+            computerTurn.textContent = '✌';
             return 'scissors';
             break;
     }
@@ -20,22 +23,32 @@ function playRound(playerSelection, computerSelection){
 
     if(playerSelection === 'rock' && computerSelection === 'scissors'){
         winCount++;
-        return winMessage;
+        scoreResult.textContent = winMessage;
+        scoreMessage.textContent = 
+            `${playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)} beats ${computerSelection}`;
     }
     else if(playerSelection === 'paper' && computerSelection === 'rock'){
         winCount++;
-        return winMessage;
+        scoreResult.textContent = winMessage;
+        scoreMessage.textContent = 
+            `${playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)} beats ${computerSelection}`;
     }
     else if(playerSelection === 'scissors' && computerSelection === 'paper'){
         winCount++;
-        return winMessage;
+        scoreResult.textContent = winMessage;
+        scoreMessage.textContent = 
+            `${playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)} beats ${computerSelection}`;
     }
     else if(playerSelection === computerSelection){
-        return 'Draw';
+        scoreResult.textContent = 'It\'s a draw';
+        scoreMessage.textContent = 
+            `${playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)} ties with ${computerSelection}`;
     }
     else {
         loseCount++;
-        return loseMessage;
+        scoreResult.textContent = loseMessage;
+        scoreMessage.textContent = 
+            `${playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)} is beaten by ${computerSelection}`;
     }
 }
 
@@ -43,7 +56,6 @@ let playerSelection;
 let computerSelection;
 let winCount = 0;
 let loseCount = 0;
-
 
 function game(){
     winCount = 0;
@@ -65,3 +77,42 @@ function game(){
 }
 
 //game();
+
+function btnClick(e){
+    if(this.getAttribute('id') === 'rock'){
+        playerTurn.textContent = '✊';
+        playerSelection = 'rock';
+        computerSelection = getComputerChoice();
+        playRound(playerSelection, computerSelection);
+        updateScore();
+    }
+    else if(this.getAttribute('id') === 'paper'){
+        playerTurn.textContent = '✋';
+        playerSelection = 'paper';
+        computerSelection = getComputerChoice();
+        playRound(playerSelection, computerSelection);
+        updateScore();
+    }
+    else {
+        playerTurn.textContent = '✌';
+        playerSelection = 'scissors';
+        computerSelection = getComputerChoice();
+        playRound(playerSelection, computerSelection);
+        updateScore();
+    }
+}
+
+function updateScore(){
+    playerScore.textContent = `Player : ${winCount}`;
+    computerScore.textContent = `Computer : ${loseCount}`;
+}
+
+const scoreResult = document.querySelector('#scoreResult');
+const scoreMessage = document.querySelector('#scoreMessage');
+const playerTurn = document.querySelector('.playerTurn');
+const computerTurn = document.querySelector('.computerTurn');
+const playerScore = document.querySelector('.playerScore');
+const computerScore = document.querySelector('.computerScore');
+const btns = document.querySelectorAll('.btn');
+
+btns.forEach(btn => btn.addEventListener('click', btnClick));
